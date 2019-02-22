215cd8e6d0b4704a1881c2abd4f8b873a6025d9e
Brian Velez <brian.velez@blackrock.com>
2019-02-22 03:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function shout(string) {
+  // returns string in all uppercase
   return string.toUpperCase();
+}
+
+function whisper(string) {
+  // returns string in all lowercase
+  return string.toLowerCase();
 }
